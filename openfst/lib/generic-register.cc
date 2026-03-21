c55bfbe69b377c70914730e7b216c887291c12dd
Alexander Gutkin <agutkin@google.com>
2026-03-21 22:01:47 +0000

    Introduce WIN32-specific APIs to generic-registerer.
    This placates the build:
    
    ```
    openfst/lib/generic-register.cc(20): fatal error C1083: Cannot open include file: 'dlfcn.h': No such file or directory
    ```
    
    PiperOrigin-RevId: 887414419

--- openfst/lib/generic-register.cc
+++ openfst/lib/generic-register.cc
@@ -14,13 +14,16 @@
 
 #include "openfst/lib/generic-register.h"
 
-#include "absl/base/nullability.h"
-
 #ifndef FST_NO_DYNAMIC_LINKING
+#if !defined(_WIN32)
 #include <dlfcn.h>
+#else
+#include <windows.h>
+#endif  // _WIN32
 #endif  // FST_NO_DYNAMIC_LINKING
 
 
+#include "absl/base/nullability.h"
 #include "absl/log/log.h"
 
 namespace fst {
@@ -30,11 +33,26 @@ bool LoadSharedObject(const char* absl_nonnull so_filename) {
 #ifdef FST_NO_DYNAMIC_LINKING
   return false;
 #else
-  void* handle = dlopen(so_filename, RTLD_LAZY);
+#if defined(_WIN32)
+  static const HMODULE handle = LoadLibrary(TEXT(so_filename));
   if (handle == nullptr) {
-    LOG(ERROR) << "GenericRegister::GetEntry: " << dlerror();
+    wchar_t buf[512];
+    FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                   nullptr, GetLastError(),
+                   MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), buf,
+                   sizeof(buf) / sizeof(wchar_t), nullptr);
+    LOG(ERROR) << "GenericRegister::GetEntry for '" << so_filename
+               << "': " << buf;
     return false;
   }
+#else
+  static const void* handle = dlopen(so_filename, RTLD_LAZY);
+  if (handle == nullptr) {
+    LOG(ERROR) << "GenericRegister::GetEntry for '" << so_filename
+               << "': " << dlerror();
+    return false;
+  }
+#endif  // WIN32
 #ifdef RUN_MODULE_INITIALIZERS
   RUN_MODULE_INITIALIZERS();
 #endif
