287a9d2049dc4ece6fea6f455a6ceb1356674e33
Jesse Rosenstock <jmr@google.com>
2026-03-26 01:47:24 -0700

    fst_test: Use gUnit
    Use gUnit test fixtures rather than one giant RunTest function.
    
    For now, we leave FstTester, but it could probably become a
    parameterized test.
    
    PiperOrigin-RevId: 889694295

--- openfst/test/fst_test.cc
+++ openfst/test/fst_test.cc
@@ -83,80 +83,9 @@ static FstRegisterer<CompactFst<StdArc, TrivialCompactor<StdArc>>>
 static FstRegisterer<CompactFst<CustomArc, TrivialCompactor<CustomArc>>>
     CompactFst_CustomArc_CustomCompactor_registerer;
 
-void RunTest() {
-  LOG(INFO) << "Testing VectorFst<StdArc>.";
-  {
-    for (const size_t num_states : {0, 1, 2, 3, 128}) {
-      FstTester<VectorFst<StdArc>> std_vector_tester(num_states);
-      std_vector_tester.TestBase();
-      std_vector_tester.TestExpanded();
-      std_vector_tester.TestAssign();
-      std_vector_tester.TestCopy();
-      std_vector_tester.TestIO();
-      std_vector_tester.TestMutable();
-    }
-
-    LOG(INFO) << "Testing empty StdVectorFst.";
-    FstTester<VectorFst<StdArc>> empty_tester(/*num_states=*/0);
-    {
-      const VectorFst<StdArc> empty_fst;
-      empty_tester.TestBase(empty_fst);
-      empty_tester.TestExpanded(empty_fst);
-      empty_tester.TestCopy(empty_fst);
-      empty_tester.TestIO(empty_fst);
-      empty_tester.TestAssign(empty_fst);
-    }
-    {
-      VectorFst<StdArc> empty_fst;
-      empty_tester.TestMutable(&empty_fst);
-    }
-  }
-
-  LOG(INFO) << "Testing ConstFst<StdArc>.";
-  {
-    FstTester<ConstFst<StdArc>> std_const_tester;
-    std_const_tester.TestBase();
-    std_const_tester.TestExpanded();
-    std_const_tester.TestCopy();
-    std_const_tester.TestIO();
-  }
-
-  LOG(INFO) << "Testing CompactArcFst<StdArc, TrivialArcCompactor<StdArc>>.";
-  {
-    FstTester<CompactArcFst<StdArc, TrivialArcCompactor<StdArc>>>
-        std_compact_tester;
-    std_compact_tester.TestBase();
-    std_compact_tester.TestExpanded();
-    std_compact_tester.TestCopy();
-    std_compact_tester.TestIO();
-  }
-
-  LOG(INFO) << "Testing CompactFst<StdArc, TrivialArcCompactor<StdArc>>.";
-  {
-    for (const size_t num_states : {0, 1, 2, 3, 128}) {
-      FstTester<CompactFst<StdArc, TrivialCompactor<StdArc>>>
-          std_compact_tester(num_states);
-      std_compact_tester.TestBase();
-      std_compact_tester.TestExpanded();
-      std_compact_tester.TestCopy();
-      std_compact_tester.TestIO();
-    }
-
-    LOG(INFO) << "Testing empty CompactFst.";
-    FstTester<CompactFst<StdArc, TrivialCompactor<StdArc>>> empty_tester(
-        /*num_states=*/0);
-    {
-      const CompactFst<StdArc, TrivialCompactor<StdArc>> empty_fst;
-      empty_tester.TestBase(empty_fst);
-      empty_tester.TestExpanded(empty_fst);
-      empty_tester.TestCopy(empty_fst);
-      empty_tester.TestIO(empty_fst);
-    }
-  }
-
-  // VectorFst<CustomArc> tests
-  {
-    FstTester<VectorFst<CustomArc>> std_vector_tester;
+TEST(FstTest, VectorFstStdArc) {
+  for (const size_t num_states : {0, 1, 2, 3, 128}) {
+    FstTester<VectorFst<StdArc>> std_vector_tester(num_states);
     std_vector_tester.TestBase();
     std_vector_tester.TestExpanded();
     std_vector_tester.TestAssign();
@@ -164,92 +93,149 @@ void RunTest() {
     std_vector_tester.TestIO();
     std_vector_tester.TestMutable();
   }
+}
 
-  // ConstFst<CustomArc> tests
+TEST(FstTest, EmptyVectorFstStdArc) {
+  FstTester<VectorFst<StdArc>> empty_tester(/*num_states=*/0);
   {
-    FstTester<ConstFst<CustomArc>> std_const_tester;
-    std_const_tester.TestBase();
-    std_const_tester.TestExpanded();
-    std_const_tester.TestCopy();
-    std_const_tester.TestIO();
-  }
-
-  // CompactArcFst<CustomArc, TrivialArcCompactor<CustomArc>>
-  {
-    for (const size_t num_states : {0, 1, 2, 3, 128}) {
-      FstTester<CompactArcFst<CustomArc, TrivialArcCompactor<CustomArc>>>
-          std_compact_tester(num_states);
-      std_compact_tester.TestBase();
-      std_compact_tester.TestExpanded();
-      std_compact_tester.TestCopy();
-      std_compact_tester.TestIO();
-    }
-
-    // TODO: Make this work.
-#if 0
-    // Test with a default-constructed Fst, not a copied Fst.
-    FstTester<CompactArcFst<CustomArc, CustomCompactor<CustomArc>>>
-        empty_tester(/*num_states=*/0);
-    const CompactArcFst<CustomArc, CustomCompactor<CustomArc>> empty_fst;
+    const VectorFst<StdArc> empty_fst;
     empty_tester.TestBase(empty_fst);
     empty_tester.TestExpanded(empty_fst);
     empty_tester.TestCopy(empty_fst);
     empty_tester.TestIO(empty_fst);
-#endif
+    empty_tester.TestAssign(empty_fst);
   }
-
-  // CompactFst<CustomArc, TrivialArcCompactor<CustomArc>>
   {
-    for (const size_t num_states : {0, 1, 2, 3, 128}) {
-      FstTester<CompactFst<CustomArc, TrivialCompactor<CustomArc>>>
-          std_compact_tester(num_states);
-      std_compact_tester.TestBase();
-      std_compact_tester.TestExpanded();
-      std_compact_tester.TestCopy();
-      std_compact_tester.TestIO();
-    }
-
-    // TODO: Add tests on default-constructed Fst.
+    VectorFst<StdArc> empty_fst;
+    empty_tester.TestMutable(&empty_fst);
   }
+}
 
-  // ConstFst<StdArc, uint16_t> tests
-  {
-    FstTester<ConstFst<StdArc, uint16_t>> std_const_tester;
-    std_const_tester.TestBase();
-    std_const_tester.TestExpanded();
-    std_const_tester.TestCopy();
-    std_const_tester.TestIO();
+TEST(FstTest, ConstFstStdArc) {
+  FstTester<ConstFst<StdArc>> std_const_tester;
+  std_const_tester.TestBase();
+  std_const_tester.TestExpanded();
+  std_const_tester.TestCopy();
+  std_const_tester.TestIO();
+}
+
+TEST(FstTest, CompactArcFstStdArcTrivialArcCompactor) {
+  FstTester<CompactArcFst<StdArc, TrivialArcCompactor<StdArc>>>
+      std_compact_tester;
+  std_compact_tester.TestBase();
+  std_compact_tester.TestExpanded();
+  std_compact_tester.TestCopy();
+  std_compact_tester.TestIO();
+}
+
+TEST(FstTest, CompactFstStdArcTrivialCompactor) {
+  for (const size_t num_states : {0, 1, 2, 3, 128}) {
+    FstTester<CompactFst<StdArc, TrivialCompactor<StdArc>>> std_compact_tester(
+        num_states);
+    std_compact_tester.TestBase();
+    std_compact_tester.TestExpanded();
+    std_compact_tester.TestCopy();
+    std_compact_tester.TestIO();
   }
+}
 
-  // CompactArcFst<StdArc, TrivialArcCompactor<StdArc>, uint16_t>
+TEST(FstTest, EmptyCompactFstStdArcTrivialCompactor) {
+  FstTester<CompactFst<StdArc, TrivialCompactor<StdArc>>> empty_tester(
+      /*num_states=*/0);
   {
-    FstTester<CompactArcFst<StdArc, TrivialArcCompactor<StdArc>, uint16_t>>
-        std_compact_tester;
+    const CompactFst<StdArc, TrivialCompactor<StdArc>> empty_fst;
+    empty_tester.TestBase(empty_fst);
+    empty_tester.TestExpanded(empty_fst);
+    empty_tester.TestCopy(empty_fst);
+    empty_tester.TestIO(empty_fst);
+  }
+}
+
+TEST(FstTest, VectorFstCustomArc) {
+  FstTester<VectorFst<CustomArc>> std_vector_tester;
+  std_vector_tester.TestBase();
+  std_vector_tester.TestExpanded();
+  std_vector_tester.TestAssign();
+  std_vector_tester.TestCopy();
+  std_vector_tester.TestIO();
+  std_vector_tester.TestMutable();
+}
+
+TEST(FstTest, ConstFstCustomArc) {
+  FstTester<ConstFst<CustomArc>> std_const_tester;
+  std_const_tester.TestBase();
+  std_const_tester.TestExpanded();
+  std_const_tester.TestCopy();
+  std_const_tester.TestIO();
+}
+
+TEST(FstTest, CompactArcFstCustomArcTrivialArcCompactor) {
+  for (const size_t num_states : {0, 1, 2, 3, 128}) {
+    FstTester<CompactArcFst<CustomArc, TrivialArcCompactor<CustomArc>>>
+        std_compact_tester(num_states);
+    std_compact_tester.TestBase();
+    std_compact_tester.TestExpanded();
+    std_compact_tester.TestCopy();
+    std_compact_tester.TestIO();
+  }
+}
+
+// TODO: Make this work.
+TEST(FstTest, DISABLED_EmptyCompactArcFstCustomArcTrivialArcCompactor) {
+  // Test with a default-constructed Fst, not a copied Fst.
+  FstTester<CompactArcFst<CustomArc, TrivialArcCompactor<CustomArc>>>
+      empty_tester(/*num_states=*/0);
+  const CompactArcFst<CustomArc, TrivialArcCompactor<CustomArc>> empty_fst;
+  empty_tester.TestBase(empty_fst);
+  empty_tester.TestExpanded(empty_fst);
+  empty_tester.TestCopy(empty_fst);
+  empty_tester.TestIO(empty_fst);
+}
+
+TEST(FstTest, CompactFstCustomArcTrivialCompactor) {
+  for (const size_t num_states : {0, 1, 2, 3, 128}) {
+    FstTester<CompactFst<CustomArc, TrivialCompactor<CustomArc>>>
+        std_compact_tester(num_states);
     std_compact_tester.TestBase();
     std_compact_tester.TestExpanded();
     std_compact_tester.TestCopy();
     std_compact_tester.TestIO();
   }
 
-  // FstTester<StdArcLookAheadFst>
-  {
-    FstTester<StdArcLookAheadFst> std_matcher_tester;
-    std_matcher_tester.TestBase();
-    std_matcher_tester.TestExpanded();
-    std_matcher_tester.TestCopy();
-  }
+  // TODO: Add tests on default-constructed Fst.
+}
 
-  // EditFst<StdArc> tests
-  {
-    FstTester<EditFst<StdArc>> std_edit_tester;
-    std_edit_tester.TestBase();
-    std_edit_tester.TestExpanded();
-    std_edit_tester.TestAssign();
-    std_edit_tester.TestCopy();
-    std_edit_tester.TestMutable();
-  }
+TEST(FstTest, ConstFstStdArcUint16) {
+  FstTester<ConstFst<StdArc, uint16_t>> std_const_tester;
+  std_const_tester.TestBase();
+  std_const_tester.TestExpanded();
+  std_const_tester.TestCopy();
+  std_const_tester.TestIO();
+}
 
-  std::cout << "PASS" << std::endl;
+TEST(FstTest, CompactArcFstStdArcTrivialArcCompactorUint16) {
+  FstTester<CompactArcFst<StdArc, TrivialArcCompactor<StdArc>, uint16_t>>
+      std_compact_tester;
+  std_compact_tester.TestBase();
+  std_compact_tester.TestExpanded();
+  std_compact_tester.TestCopy();
+  std_compact_tester.TestIO();
+}
+
+TEST(FstTest, StdArcLookAheadFst) {
+  FstTester<StdArcLookAheadFst> std_matcher_tester;
+  std_matcher_tester.TestBase();
+  std_matcher_tester.TestExpanded();
+  std_matcher_tester.TestCopy();
+}
+
+TEST(FstTest, EditFstStdArc) {
+  FstTester<EditFst<StdArc>> std_edit_tester;
+  std_edit_tester.TestBase();
+  std_edit_tester.TestExpanded();
+  std_edit_tester.TestAssign();
+  std_edit_tester.TestCopy();
+  std_edit_tester.TestMutable();
 }
 
 }  // namespace
@@ -258,5 +244,5 @@ void RunTest() {
 int main(int argc, char** argv) {
   absl::SetFlag(&FLAGS_fst_verify_properties, true);
   ::testing::InitGoogleTest(&argc, argv);
-  fst::RunTest();
+  return RUN_ALL_TESTS();
 }
