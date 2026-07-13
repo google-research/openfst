5c2e404c5bd0d7d804dbfbaacfdc5947432118cc
OpenFst Team <no-reply@google.com>
2026-07-13 00:43:40 -0700

    Automated Code Change
    PiperOrigin-RevId: 946849825

--- openfst/lib/verify.h
+++ openfst/lib/verify.h
@@ -43,6 +43,8 @@ absl::Status VerifyWithStatus(const Fst<Arc>& fst,
   const auto ns = CountStates(fst);
   if (start == kNoStateId && ns > 0) {
     return absl::InvalidArgumentError("Verify: FST start state ID not set");
+  } else if (start < 0 && start != kNoStateId) {
+    return absl::InvalidArgumentError("Verify: FST start state ID is negative");
   } else if (start >= ns) {
     return absl::InvalidArgumentError(
         "Verify: FST start state ID exceeds number of states");
